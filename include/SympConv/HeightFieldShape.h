bd023a8452135b3733ffe621bcd704a1723d0cc4
liulikui <liulikui@gmail.com>
2026-03-10 16:04:37 +0800

    完善HeightFieldShape的定义

--- include/SympConv/HeightFieldShape.h
+++ include/SympConv/HeightFieldShape.h
@@ -1,35 +1,18 @@
-#ifndef SYMPCONV_SHAPE_H
-#define SYMPCONV_SHAPE_H
+#ifndef SYMPCONV_HEIGHT_FIELD_SHAPE_H
+#define SYMPCONV_HEIGHT_FIELD_SHAPE_H
 
-#include "Config.h"
+#include "Shape.h"
 
 namespace SympConv {
 
-enum class EShapeType : uint8_t
-{
-	Plane,
-	Convex,
-	Mesh,
-	HeightField,
-
-	User1,
-	User2,
-	User3,
-};
-
 template<typename T>
-class TShape
+class THeightFieldShape : public TShape<T>
 {
 public:
-	TShape(EShapeType InShapeType) :
-		mShapeType(InShapeType){}
-
-	EShapeType GetShapeType() const { return mShapeType; }
-
-private:
-	EShapeType mShapeType;
+	THeightFieldShape() :
+		TShape<T>(EShapeType::HeightField){}
 };
 
 } // namespace SympConv
 
-#endif // SYMPCONV_PLANE_H
+#endif // SYMPCONV_HEIGHT_FIELD_SHAPE_H
